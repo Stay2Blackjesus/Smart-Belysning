c269e2a18bdfc9f6a79e8dd186d2b034426ac77b
Kais Atta Torkmani <tishel19kaitor@innovationsgymnasiet.se>
2021-03-23 18:04:00 +0100

    Update 1.2

--- Script.js
+++ Script.js
@@ -6,3 +6,10 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
+
+//Ändrar Bakgrundsfärg till Dark-Mode värdet från Style.css
+function Btn1() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
